a18e79c914e1ebe2f1dbd2cae704351ae12484a4
mediwateyash <diwateyash2004@gmail.com>
2026-06-19 00:37:27 +0530

    Fix Express 5 routing issue on Render

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.dirname(__filename)
   
   app.use(express.static(path.join(__dirname, '../client/dist')))
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/dist/index.html'))
   })
 } else {
